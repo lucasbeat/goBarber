f6fad8657ab2facc626984dcad41fb464a379823
lucas beat <clashbruno10@gmail.com>
2020-02-27 13:20:44 -0300

    feat: add private routes

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <div />;
+  return <h1>SignIn</h1>;
 }
