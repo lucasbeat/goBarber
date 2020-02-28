1a1fcfb78007c0ff4af8598008b95e7ffd8405c2
lucas beat <clashbruno10@gmail.com>
2020-02-28 12:11:50 -0300

    fix: import bug fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,10 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
-    'import/resolver': 'webpack',
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
   },
 };
