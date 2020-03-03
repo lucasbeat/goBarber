6c61c45f34391e9a668c634636fd8bfdcf40874e
lucas beat <clashbruno10@gmail.com>
2020-03-02 23:39:06 -0300

    feat: storing profile

---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,16 @@
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  profile: null,
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.profile = action.payload.profile;
+      });
+    default:
+      return state;
+  }
+}
