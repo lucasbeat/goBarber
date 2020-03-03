039048f6a99aa4753e7c1ba83517c7e188c2deda
lucas beat <clashbruno10@gmail.com>
2020-03-03 00:16:28 -0300

    feat: authentication loading

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -8,7 +8,7 @@ export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
-        draft.profile = action.payload.profile;
+        draft.profile = action.payload.user;
       });
     default:
       return state;
